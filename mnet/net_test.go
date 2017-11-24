509a89bf4b2e89a962481cadef777e46dd346b0e
Alexander Ewetumo <trinoxf@gmail.com>
2017-11-24 14:14:06 +0100

    Swap net for mnet pkgname

---
+++ mnet/net_test.go
@@ -0,0 +1,38 @@
+package mnet_test
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/influx6/faux/tests"
+	"github.com/influx6/melon/mnet"
+)
+
+func TestReadWriter(t *testing.T) {
+	reader, err := mnet.Listen("tcp", ":4050", nil)
+	if err != nil {
+		tests.FailedWithError(err, "Should have successfully created reader and writer")
+	}
+	tests.Passed("Should have successfully created reader and writer")
+
+	defer reader.Close()
+
+	go makeConn(":4050")
+	conn, err := reader.ReadConn()
+	if err != nil {
+		tests.FailedWithError(err, "Should have sucessfully read connection")
+	}
+	tests.Passed("Should have sucessfully read connection")
+
+	reader.WriteConn(conn)
+}
+
+func makeConn(addr string) {
+	conn, err := net.DialTimeout("tcp", addr, 30*time.Second)
+	if err != nil {
+		return
+	}
+
+	conn.Close()
+}
